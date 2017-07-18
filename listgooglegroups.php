1e5497648c06a269760f6a43611ad58f7255be92
naseemkullah <naseemkullah@gmail.com>
2017-07-18 09:45:28 -0400

    Update listgooglegroups.php

--- listgooglegroups.php
+++ listgooglegroups.php
@@ -1,4 +1,5 @@
 //replace my_domain with a domain
+//configure redirect uri to point to oauth2callback.php
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 session_start();
@@ -10,7 +11,7 @@ $client->addScope('https://www.googleapis.com/auth/admin.directory.user');
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
 } else {
-  $redirect_uri = '';
+  $redirect_uri = 'https://path.to/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 
