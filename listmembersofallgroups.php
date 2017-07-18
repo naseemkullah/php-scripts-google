d4d573d0fb83e1389b3207ebd756009e8f01b94b
naseemkullah <naseemkullah@gmail.com>
2017-07-18 09:44:51 -0400

    Update listmembersofallgroups.php

--- listmembersofallgroups.php
+++ listmembersofallgroups.php
@@ -1,16 +1,17 @@
 //replace 'my_domain' with a domain
+//configure redirect uri to point to oauth2callback.php
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 session_start();
 $client = new Google_Client();
 $client->setAuthConfig('client_secrets.json');
 $client->addScope('https://www.googleapis.com/auth/admin.directory.group');
-$client->addScope('https://www.googleapis.com/auth/admin.directory.user');
+
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
 } else {
-  $redirect_uri = 'https://ops.cloudops.net/googlegroups/oauth2callback.php';
+  $redirect_uri = 'https://path.to/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 
